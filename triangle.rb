b7c3895c56a6c2a094c057ccd3b377611f795ea4
Igor_Shum <Shumeikoigor@yandex.ru>
2020-10-21 16:48:03 +0300

    при возведении в степень пробелы вокруг оператора не ставятся

--- triangle.rb
+++ triangle.rb
@@ -25,7 +25,7 @@ cat1, cat2 = mass.min(2)
 if (gip == cat1 && gip == cat2)
 	p "Равносторонний треугольник1"
 else
-	if gip ** 2 == (cat1 ** 2 + cat2 ** 2)
+	if gip**2 == (cat1**2 + cat2**2)
 			p "Прямоугольный треугольник"
 	else
 		if (gip == cat1 || gip == cat2)
