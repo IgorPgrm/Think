f7049cdc3a445aee8ae5b119710dfdde81117f7e
Igor <Shumeikoigor@yandex.ru>
2020-10-21 22:33:55 +0300

    Update ideal_weight.rb
    Ручное исправление через сайт гитхаб

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -7,7 +7,7 @@ height = gets.chomp.to_f
 ideal_weight = (height.to_f - 110) * 1.15
 
 if ideal_weight < 0
-	p "Ваш вес уже оптимальный"
+  p "Ваш вес уже оптимальный"
 else
-	p "Идеальный вес для роста #{height}см, составляет #{ideal_weight} кг"
+  p "Идеальный вес для роста #{height}см, составляет #{ideal_weight} кг"
 end
