f7f2c4f8357dc6ea7471feaea3d7e270f62322aa
Igor <Shumeikoigor@yandex.ru>
2020-10-21 22:36:13 +0300

    Update discr.rb
    Ручное исправление через сайт гитхаб

--- discr.rb
+++ discr.rb
@@ -5,25 +5,20 @@ b = gets.chomp.to_f
 p "Введите c="
 c = gets.chomp.to_f
 
-discr = b ** 2 - 4 * a * c
+discr = b**2 - 4 * a * c
 p "Дискриминант = #{discr}"
 
 if discr < 0
-	p "Дискриминант меньше нуля. Корней нет"
+  p "Дискриминант меньше нуля. Корней нет"
 elsif discr == 0
-	p "Дискриминант равен нулю! Оба корня одинаковы x1 = x2"
-	x =- b/(2 * a)
-	p "X1 == X2 = #{x}"
+  p "Дискриминант равен нулю! Оба корня одинаковы x1 = x2"
+  x = -b / (2 * a)
+  p "X1 == X2 = #{x}"
 else
-	sqr_discr = Math.sqrt(discr)
-	p "Дискриминант больше нуля!" 
-	x1 = (-b + sqr_discr) / 2 * a
-	p "X1 = #{x1}"
-	x2 = (-b - sqr_discr) / 2 * a
-	p "X2 = #{x2}"
+  sqr_discr = Math.sqrt(discr)
+  p "Дискриминант больше нуля!" 
+  x1 = (-b + sqr_discr) / 2 * a
+  p "X1 = #{x1}"
+  x2 = (-b - sqr_discr) / 2 * a
+  p "X2 = #{x2}"
 end
-
-=begin
-	>> 3 4 4
-	x = 2
-=end
