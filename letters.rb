df9a1986e5f14619ea12f099f0e449d39b780f63
Igor_Shum <Shumeikoigor@yandex.ru>
2020-10-22 18:13:45 +0300

    Заполнение хеша гласными буквами

--- letters.rb
+++ letters.rb
@@ -1,1 +1,16 @@
+# Заполнить хеш гласными буквами, где значением будет являтся
+# порядковый номер буквы в алфавите (a - 1).
+# «A», «E», «I», «O», «U», «Y».
 
+glasn = %w[a e i o u y]
+hash = Hash.new
+alphabet = []
+alphabet = ('a'..'z').to_a
+
+alphabet.each_with_index do |a, index|
+  if glasn.include?(a)
+    hash[a.to_sym] = index+1
+  end
+end
+
+puts hash.inspect
