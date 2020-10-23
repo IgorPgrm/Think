a6bc3c7103057ecc93cda76e40fe1f09ab74824f
Igor_Shum <Shumeikoigor@yandex.ru>
2020-10-23 17:25:21 +0300

    Вывод массива с числами до 100

--- Task2/fibonacci.rb
+++ Task2/fibonacci.rb
@@ -1,14 +1,18 @@
 # Заполнить массив числами фибоначчи до 100
 mass = []
+counter = 0
 
 def fibonacci numb
   return numb if numb < 2
   fibonacci(numb-2) + fibonacci(numb-1)
 end
 
-(0..100).each do |n|
-  mass << fibonacci(n)
+loop do
+  numb = fibonacci counter
+  break if numb >= 100
+  mass << numb
+  counter += 1
 end
 
-puts mass
+puts mass.inspect
 
