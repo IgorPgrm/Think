0b5cca6f4c77a2ab21ea57ea482fdb9f0d921679
Igor_Shum <Shumeikoigor@yandex.ru>
2020-10-25 16:29:50 +0300

    Переписан, хранение в массиве

--- Task3/station.rb
+++ Task3/station.rb
@@ -1,38 +1,26 @@
-#Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
-#Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
-
 class Station
 
   def initialize(title)
     @title = title
-
-    @trains = {}
+    @trains = []
     @train_index = 0
   end
 
-  def add_train train #принимает объект поезд
+  def add_train train
     puts train.inspect
-    @trains["id#{@train_index}".to_sym] = { train: train }
+    @trains << train
     @train_index += 1
   end
 
-  def train_departure #отправление поезда
-
+  def remove_train train
+    @trains.delete(train)
   end
 
   def show_trains
-    @trains.each do |key, trains|
-      trains.each do |key, value|
-        puts "\t\t #{value.number}\t| #{value.type}\t| #{value.carriages}"
-      end
-    end
+    @trains.each { |train| puts "\t\t #{train.number}\t| #{train.type}\t| #{train.carriages}" }
   end
 
   def show_trains_by_type type #отображение по типу
-    @trains.each do |key, trains|
-      trains.each do |key, value|
-        puts "\t\t #{value.number}\t| #{value.type}\t| #{value.carriages}" if value.type == type.to_sym
-      end
-    end
+    @trains.each { |train| puts "\t\t #{train.number}\t| #{train.type}\t| #{train.carriages}"  if train.type == type}
   end
 end
