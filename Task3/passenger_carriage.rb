238283a463c1575904bc98a12895dcef8b74c0f5
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-04 15:40:58 +0300

    +Метод добавления пассажира

--- Task3/passenger_carriage.rb
+++ Task3/passenger_carriage.rb
@@ -1,12 +1,25 @@
 require_relative 'carriage'
 
 class PassengerCarriage < Carriage
-  attr_accessor :number_seats
-  attr_reader :residue_number
+  attr_reader :total_count, :places, :free_places
 
-  def initialize
+  def initialize(residue_count)
+    @total_count = residue_count
+    @free_places = residue_count
+    @places = []
     super(:passenger)
-    @residue_number = 54 #свободные места
-    @number_seats = 0 #занято мест
+    @total_count.times { @places << :free } unless @total_count == 0
   end
+
+  def add_passenger
+    raise ArgumentError, "Нет свободных мест" if @free_places.zero?
+    @places.each_with_index do |place, index|
+      if place == :free
+        @places[index] = :passenger
+        @free_places -= 1
+        break
+      end
+    end
+  end
+
 end
