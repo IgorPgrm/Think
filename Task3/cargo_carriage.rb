0a0d8a49b17032ac381fbf4b8601c793c53ff72e
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-04 16:20:23 +0300

    Переписан класс Добавлен метод загрузки Информация вагона, общий\свободный\занятый вес

--- Task3/cargo_carriage.rb
+++ Task3/cargo_carriage.rb
@@ -1,19 +1,20 @@
 require_relative 'carriage'
 
 class CargoCarriage < Carriage
-  attr_accessor :capacity, :volume #масса полезного груза вагона
-  CARGO_CAPACITY = 71_000 # максимально перевозимая масса
-  CARGO_VOLUME = 88 # объём в кубах
-  CARGO_MASS = 24_000 #масса вагона 24 т.
+  attr_reader :total_volume, :free_volume, :busy_volume
 
-  def initialize
+  def initialize(volume)
     super(:cargo)
-    capacity = 0
-    volume = 0
+    @total_volume = volume
+    @free_volume = volume
+    @busy_volume = 0
   end
 
-  def load mass=0, volume=0
-
+  def load volume
+    raise ArgumentError, "В вагоне нет места, он загружен" if @busy_volume == @total_volume
+    raise ArgumentError, "Большой объём для размещения! Укажите меньший объём" if (@busy_volume+volume > @total_volume)
+    @free_volume -= volume
+    @busy_volume += volume
   end
 
 end
