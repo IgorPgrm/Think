36e3e16eaf0cbc74c4ce4ef01ba27f282e0cb632
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-06 17:19:07 +0300

    Исправления для вагонов

--- Task3/passenger_carriage.rb
+++ Task3/passenger_carriage.rb
@@ -9,19 +9,19 @@ class PassengerCarriage < Carriage
     @busy_places = 0
     @places = []
     super(:passenger)
-    @total_count.times { @places << :free } unless @total_count == 0
+    @total_count.times { @places << :free } unless @total_count.zero?
   end
 
   def add_passenger
-    raise ArgumentError, "Нет свободных мест" if @free_places.zero?
+    raise ArgumentError, 'Нет свободных мест' if @free_places.zero?
+
     @places.each_with_index do |place, index|
-      if place == :free
-        @places[index] = :passenger
-        @busy_places += 1
-        @free_places = @total_count - @busy_places
-        break
-      end
+      next unless place == :free
+
+      @places[index] = :passenger
+      @busy_places += 1
+      @free_places = @total_count - @busy_places
+      break
     end
   end
-
 end
