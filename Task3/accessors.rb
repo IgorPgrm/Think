898619a97d45df5547939a7b8876e9686fe66d6e
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-09 20:42:49 +0300

    +Добавлен attr_accessor_with_history, добавление инстанс гет и сет

--- Task3/accessors.rb
+++ Task3/accessors.rb
@@ -1,8 +1,20 @@
 module Accessors
-  module ClassMethods
-  def attr_accessor_with_history(*args)
-
+  def self.included(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
   end
+
+  module ClassMethods
+
+  def attr_accessor_with_history(*names)
+    names.each do |name|
+      method_at = "@#{name}".to_sym
+      method_history = "@#{name}_history"
+      define_method(name){instance_variable_get(method_at)}
+      define_method("#{name}="){|value| instance_variable_set method_at, value}
+    end
+  end
+
   end
 
   module InstanceMethods
