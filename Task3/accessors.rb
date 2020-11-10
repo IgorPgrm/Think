b90d7bc836d90a3152ca01f7cec4db0384874875
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-10 15:35:46 +0300

    Attr_accessor with history

--- Task3/accessors.rb
+++ Task3/accessors.rb
@@ -8,10 +8,25 @@ module Accessors
 
   def attr_accessor_with_history(*names)
     names.each do |name|
-      method_at = "@#{name}".to_sym
-      method_history = "@#{name}_history"
-      define_method(name){instance_variable_get(method_at)}
-      define_method("#{name}="){|value| instance_variable_set method_at, value}
+      method_var = "@#{name}".to_sym
+      class_eval %Q{
+      def #{name}
+        @#{name} || nil
+      end
+
+      def #{name}=(value)
+        @#{name} = value
+      end
+
+      def #{name}_history
+        @#{name}_history || [nil] # give default value if not assigned
+      end
+
+      def #{name}=(new_value)
+        @#{name}_history ||= [nil] # shortcut, compare to your line
+        @#{name}_history << @#{name} = new_value
+      end
+      }
     end
   end
 
