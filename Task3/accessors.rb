5dd5dc204c90fd84c1f44b272cad1cefb26cd3c2
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-10 17:46:39 +0300

    strong_attr_accessor

--- Task3/accessors.rb
+++ Task3/accessors.rb
@@ -5,11 +5,10 @@ module Accessors
   end
 
   module ClassMethods
-
-  def attr_accessor_with_history(*names)
-    names.each do |name|
-      method_var = "@#{name}".to_sym
-      class_eval %Q{
+    def attr_accessor_with_history(*names)
+      names.each do |name|
+        raise TypeError.new('Method name is not symbol.') unless name.is_a?(Symbol)
+        class_eval <<-CEV, __FILE__, __LINE__ + 1
       def #{name}
         @#{name} || nil
       end
@@ -26,13 +25,20 @@ module Accessors
         @#{name}_history ||= [nil] # shortcut, compare to your line
         @#{name}_history << @#{name} = new_value
       end
-      }
+        CEV
+      end
+    end
+
+    def strong_attr_accessor(method, _class)
+      raise TypeError.new('Method name is not symbol!') unless method.is_a?(Symbol)
+      define_method(method){ instance_variable_get("@#{method}") }
+      define_method("#{method}=") do |val|
+        raise TypeError "Value type is not #{_class}." unless val.is_a? _class
+        instance_variable_set("@#{method}", val)
+      end
     end
   end
 
-  end
-
   module InstanceMethods
-
   end
 end
