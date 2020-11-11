0c9a7a087040219f2ed23201e44ee17826952fea
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-11 15:49:53 +0300

    +InstanceMethod validate!

--- Task3/validation_module.rb
+++ Task3/validation_module.rb
@@ -1,1 +1,40 @@
+module Validation
+  def self.included(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
+  end
+
+  module ClassMethods
+    def validation(name, type, *args)
+      @validate ||= []
+      @validate << {name: name, type: type, args: args}
+    end
+  end
+
+  module InstanceMethods
+    #validate :name, :presence
+    #validate :name, :format, /A-Z/
+    #validate :name, :type, String
+
+    def validate!
+      self.class.instance_variable_get(@validate).each do |var|
+        name = var[:name]
+        type = var[:type]
+        arg = var[:args]
+        send "#{type}", name, arg
+      end
+    end
+
+    def valid?; validate! ? true : false; end
+
+    def precence(name, arg)
+      
+    end
+
+    def format(name,arg)
+
+    end
+
+  end
+end
 
