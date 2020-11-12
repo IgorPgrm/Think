1c5df2aa2530d76d8ed9f7045b2346cd48f1ada7
Igor_Shum <Shumeikoigor@yandex.ru>
2020-11-12 15:13:31 +0300

    Validation length принимает min: max:

--- Task3/validation_module.rb
+++ Task3/validation_module.rb
@@ -5,36 +5,62 @@ module Validation
   end
 
   module ClassMethods
-    def validation(name, type, *args)
+    def validate(attr, type, *args)
       @validate ||= []
-      @validate << {name: name, type: type, args: args}
+      @validate << { attr: attr, type: type, args: args }
+      puts "Class-validate: #{@validate}"
     end
   end
 
   module InstanceMethods
-    #validate :name, :presence
-    #validate :name, :format, /A-Z/
-    #validate :name, :type, String
+    # validate :name, :presence
+    # validate :name, :format, /A-Z/
+    # validate :name, :type, String
+
+    def valid?
+      validate! ? true : false
+    end
+
+    private
 
     def validate!
-      self.class.instance_variable_get(@validate).each do |var|
-        name = var[:name]
+      self.class.instance_variable_get('@validate').each do |var|
+        attr = var[:attr]
         type = var[:type]
-        arg = var[:args]
-        send "#{type}", name, arg
+        attr_var = instance_variable_get("@#{attr}")
+        arg = var[:args][0]
+        send type.to_s, attr, attr_var, arg
       end
     end
 
-    def valid?; validate! ? true : false; end
-
-    def precence(name, arg)
-      
+    def presence(attr, value, _arg)
+      raise ArgumentError, "#{attr} is empty or nil!" if value.nil? || value.empty?
     end
 
-    def format(name,arg)
-
+    def format(attr, value, regexp)
+      raise RegexpError, "Format of #{attr} should be #{regexp}" if value !~ regexp
     end
 
+    def type(attr, value, arg)
+      raise ArgumentError, "#{attr} type must be #{arg}!" if value.class != arg
+    end
+
+    def length(attr, value, arg)
+      raise ArgumentError, "#{attr} not define" if value.nil? || value.empty?
+
+      raise ArgumentError, "#{attr} should be min: or max: value" if arg.nil?
+
+      if !arg[:min].nil?
+        min = arg[:min]
+        min_t = "min: #{min}"
+        value_min = value.length < min
+      elsif !arg[:max].nil?
+        max = arg[:max]
+        max_t = "max: #{max}"
+        value_max = value.length > max
+      end
+
+      raise ArgumentError, "#{attr} length should be #{min_t} #{max_t} chars" if value_max || value_min
+    end
   end
 end
-
